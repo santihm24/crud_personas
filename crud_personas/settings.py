67f7e98709df3c75bbee62526f0b6f3bf2a43a9b
appvisionlembo <appvisionlembo@gmail.com>
2025-04-23 10:54:54 -0500

    cambios

--- crud_personas/settings.py
+++ crud_personas/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure--!=m5f25v1_x$flvx-ie^5gp9i#k4gep)_*%p*$ox1+f%1e-i6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['proyecto-django-bt6p.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['crud-personas-wxdt.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'personas',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
